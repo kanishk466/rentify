8284d4d115a3aee65c07c34ed33a0b02694078ff
kanishk466 <kanishkas466@gmail.com>
2024-05-24 11:44:03 +0530

    update the files

--- src/App.js
+++ src/App.js
@@ -1,39 +1,11 @@
-import React, {useState} from 'react';
-import './App.css';
-import Header from './components/Header/Header';
-import LoginForm from './components/LoginForm/LoginForm';
-import RegistrationForm from './components/RegistrationForm/RegistrationForm';
-import Home from './components/Home/Home';
-import PrivateRoute from './utils/PrivateRoute';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
-import AlertComponent from './components/AlertComponent/AlertComponent';  
-function App() {
-  const [title, updateTitle] = useState(null);
-  const [errorMessage, updateErrorMessage] = useState(null);
-  return (
-    <Router>
-    <div className="App">
-      <Header title={title}/>
-        <div className="container d-flex align-items-center flex-column">
-       
+import React from 'react'
 
-          <Routes>
-           
-            <Route path='/register'  element={ <RegistrationForm showError={updateErrorMessage} updateTitle={updateTitle}/>}/>
-            <Route path="/login" element={ <LoginForm showError={updateErrorMessage} updateTitle={updateTitle}/>} />
-            <PrivateRoute>
-              <Route path='/home' element={<Home/>}/>
-            </PrivateRoute>
-          </Routes>
-          <AlertComponent errorMessage={errorMessage} hideError={updateErrorMessage}/>
-        </div>
+const App = () => {
+  return (
+    <div>
+      welcome to app
     </div>
-    </Router>
-  );
+  )
 }
 
-export default App;
+export default App
